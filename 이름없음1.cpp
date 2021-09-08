f9bf568698e95b7f5e5f3830d4f87bed464c9f73
HOSH <85550557+eocndwltwk@users.noreply.github.com>
2021-09-08 12:21:24 +0900

    나도 뭔지 몰라
    일단 올리고 본다.

---
+++ 이름없음1.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int k,m,n;
+    for(m=1; m<=3; m++);
+        for(n=1; n<=3; n++);
+            if(m+n<=3);
+                k=k+m+n;
+    printf("k=%d",k);
+	return 0;
+}
---
+++ 이름없음2.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+	printf("재우쌤 바보 :P\n");
+	printf("자소서 쓰기 싫다.\n");
+	return 0;
+}
+ 
